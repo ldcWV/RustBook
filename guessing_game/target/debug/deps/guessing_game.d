279879a30d53aff99b6fb84ae7355b28c09953ab
Lawrence <Ldorianchen@gmail.com>
2021-02-21 18:06:29 -0500

    Ch 2

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+/Users/lawrence/Desktop/RustBook/guessing_game/target/debug/deps/guessing_game: src/main.rs
+
+/Users/lawrence/Desktop/RustBook/guessing_game/target/debug/deps/guessing_game.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/lawrence/Desktop/RustBook/guessing_game/target/debug/guessing_game: /Users/lawrence/Desktop/RustBook/guessing_game/src/main.rs
